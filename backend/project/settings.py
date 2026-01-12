6aa2144e2b0bd2c04be971361783e68934162cb1
BA <122610348+VSAB69@users.noreply.github.com>
2026-01-12 08:06:04 +0530

    Update settings.py for recording upload bug

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -55,7 +55,14 @@ INSTALLED_APPS = [
 ]
 
 
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
 
 AWS_ACCESS_KEY_ID = os.getenv("R2_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("R2_SECRET_ACCESS_KEY")
